8fec58209104eabff8bc5b9568bc4f3eb828b7d3
James Kuang <james.kuang.13@gmail.com>
2022-01-20 23:03:52 -0500

    Choosing the qualifier for what counts as an election of interest

--- IdentifyResearchable.R
+++ IdentifyResearchable.R
@@ -1,9 +1,28 @@
 library(readxl)
 library(tidyverse)
+library("xlsx")
+
 
 path <- "Ranking_Frequency"
 years <- list.files(path)
+write.xlsx(data.frame(Empty = "", row.names = NULL), file = "SpecialElections.xlsx", 
+           sheetName= "blank")
+## This Pulls Each Election and Finds the Special Ones
 for (year in years) {
-  excel_sheets(paste(path, year, sep = "\\"))
-  
+  excel_path <- paste(path, year, sep = "\\")
+  elections <- excel_sheets(excel_path)
+  for (election in elections){
+    if (election != "blank"){
+      print(election)
+      result <- read_excel(path = excel_path, sheet = election) %>% 
+        select(c(2, 3)) %>%
+        rename("Ranking" = "Var1") %>% 
+        arrange(desc(Freq))
+      ratio <- result[[2, 2]]/result[[3, 2]]
+      if (ratio > 2) {
+        write.xlsx(data.frame(result), file = "SpecialElections.xlsx", 
+                   sheetName = gsub(" ", "", paste0(substr(year, 1, 4), election)), append = TRUE, row.names = FALSE)
+      }
+    }
+  }
 }
